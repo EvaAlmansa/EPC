fefc90df0791f800c56ce572b2aa7d5f651f9b0a
Jesús Chamorro <jesus@decsai.ugr.es>
2019-02-26 17:07:38 +0100

    Making improvements to event classes

--- src/events/TimeListener.java
+++ src/events/TimeListener.java
@@ -3,10 +3,21 @@ package events;
 import java.util.EventListener;
 
 /**
- *
+ * Interface that all time event listener must extend.
+ * 
  * @author Jesús Chamorro Martínez (jesus@decsai.ugr.es)
  */
 public interface TimeListener extends EventListener{
+    /**
+     * The time has changed.
+     * 
+     * @param evt the time event.
+     */
     public void timeChange(TimeEvent evt);
+    /**
+     * The time is out.
+     * 
+     * @param evt the time event.
+     */
     public void timeOut(TimeEvent evt);    
 }
