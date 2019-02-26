cdba2a4f05e4f63f3e4326e5b72e50bb4c656a76
Jesús Chamorro <jesus@decsai.ugr.es>
2019-02-26 17:01:08 +0100

    Making improvements to event classes

--- src/events/TimeEvent.java
+++ src/events/TimeEvent.java
@@ -11,26 +11,36 @@ public class TimeEvent extends EventObject {
     /**
      * Time (in time units) associated to this event.
      */
-    private long time;
+    private final long time;
     /**
      * Estimation of the time series value at this time.
      */
-    private Double estimation;
+    private final Double estimation;
     /**
      * Membeship degree to 'danger' at this time.
      */
-    private Double danger_degree;
+    private final Double danger_degree;
     /**
      * Tendency of the time series calculated as the mean over the values behind
      * this time
      */
-    private Double mean_behind;
+    private final Double mean_behind;
     /**
      * New value for the time series at this time (result of an adjustment
      * process)
      */
-    private Double adjusted_value;
+    private final Double adjusted_value;
 
+    /**
+     * Constructs a new time event.
+     * 
+     * @param source the object on which the event initially occurred
+     * @param time time (in time units) associated to this event
+     * @param estimation estimation of the time series value at this time.
+     * @param danger_degree membeship degree to 'danger' at this time.
+     * @param mean_behind tendency value of the time series at this time.
+     * @param adjusted_value new value for the time series at this time
+     */
     public TimeEvent(Object source, long time, double estimation, double danger_degree, double mean_behind, double adjusted_value) {
         super(source);
         this.time = time;
@@ -40,46 +50,47 @@ public class TimeEvent extends EventObject {
         this.adjusted_value = adjusted_value;
     }
     
+    /**
+     * Returns the time associated to this event.
+     * @return 
+     */
     public long getTime() {
         return time;
     }
-    
-    public void setTime(long time) {
-        this.time = time;
-    }    
 
-
+    /**
+     * Returns the estimation of the time series value associated to this event.
+     * 
+     * @return the estimation of the time series value
+     */
     public Double getEstimationValue() {
         return estimation;
     }
 
-    public void setEstimationValue(Double estimation) {
-        this.estimation = estimation;
-    }
-
+    /**
+     * Returns the danger degree associated to this event.
+     * 
+     * @return the danger degree.
+     */
     public Double getDangerDegree() {
         return danger_degree;
     }
 
-    public void setSlopeDegree(Double degree) {
-        this.danger_degree = degree;
-    }
-
+    /**
+     * Retunrs the new value for the time series associated to this event.
+     * 
+     * @return the new adjusted value.
+     */
     public Double getAdjustedValue() {
         return adjusted_value;
     }
 
-    public void setAdjustedValue(Double value) {
-        this.adjusted_value = value;
-    }
-
+    /**
+     * Tendency of the time series associated to this event.
+     * 
+     * @return the tendency of the time series.
+     */
     public Double getMeanBehind() {
         return mean_behind;
     }
-
-    public void setMeanBehind(Double mean_behind) {
-        this.mean_behind = mean_behind;
-    }
-    
-    
 }
